c2d2d6d74c1686382449c3a557b20fe16b69b060
jacolpn <jacolpn@outlook.com>
2020-08-21 23:53:43 -0300

    added new start page js

--- scripts/start-script.js
+++ scripts/start-script.js
@@ -1,57 +1,74 @@
-const btnStart = document.querySelector(".btn-start");
-const closeStart = document.querySelector(".close-start");
-const wrapperStart = document.querySelector(".wrapper-start");
-const sectionStart = document.querySelector(".section-start");
-// const sectionTabs = document.querySelector(".section-tabs");
-// const sectionLoading = document.querySelector(".loading-effect");
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
 
-closeStart.addEventListener("click", event => {
-  sectionStart.classList.add("form-close-start");
+function StartPage() {
+  const html = {
+    btnStart: $(".btn-start"),
+    closeStart: $(".close-start"),
+    wrapperStart: $(".wrapper-start"),
+    sectionStart: $(".section-start"),
+    fields: [...$$(".input-start")],
+    formHidden: $(".form-hide-start"),
+    body: $("body"),
+  }
   
-  sectionStart.addEventListener("animationend", event => {
-    if(event.animationName == "close-start")
-      sectionStart.style.display = "none";
-  });
-});
+  function init() {
+    html.closeStart.addEventListener("click", event => {
+      html.sectionStart.classList.add("form-close-start");
+      
+      html.sectionStart.addEventListener("animationend", event => {
+        if(event.animationName == "close-start")
+        html.sectionStart.style.display = "none";
+      });
+    });
 
-btnStart.addEventListener("click", event => {
-  event.preventDefault();
-  
-  const fields = [...document.querySelectorAll(".input-start")]
-  fields.forEach(field => {
-    if (field.value == "")
-    wrapperStart.classList.add("validate-error-start")
-  });
+    html.btnStart.addEventListener("click", event => {
+      event.preventDefault();
 
-  const formError = document.querySelector(".validate-error-start");
-  if (formError) {
-    formError.addEventListener("animationend", event => {
-      if (event.animationName == "nono-start") {
-        formError.classList.remove("validate-error-start");
+      html.fields.forEach(field => {
+        if (field.value == "")
+        html.wrapperStart.classList.add("validate-error-start")
+      });
+
+      const formError = document.querySelector(".validate-error-start");
+      if (formError) {
+        formError.addEventListener("animationend", event => {
+          if (event.animationName == "nono-start") {
+            formError.classList.remove("validate-error-start");
+          }
+        });
+      } else {
+        html.sectionStart.classList.add("form-hide-start");
+      }
+
+      
+      if (html.formHidden) {
+        html.formHidden.addEventListener("animationend", event => {
+          // sectionStart.parentNode.removeChild(sectionStart);
+          // sectionLoading.style.display = "flex";
+          // sectionTabs.style.display = "flex";
+        })
       }
     });
-  } else {
-    sectionStart.classList.add("form-hide-start");
-  }
 
-  const formHidden = document.querySelector(".form-hide-start");
-  if (formHidden) {
-    formHidden.addEventListener("animationend", event => {
-      // sectionStart.parentNode.removeChild(sectionStart);
-      // sectionLoading.style.display = "flex";
-      // sectionTabs.style.display = "flex";
-    })
-  }
-});
+    html.sectionStart.addEventListener("animationstart", event => {
+      if(event.animationName == "down-start") {
+        html.body.style.overflow = "hidden";
+      }
+    });
 
-sectionStart.addEventListener("animationstart", event => {
-  if(event.animationName == "down-start") {
-    document.querySelector("body").style.overflow = "hidden";
+    html.sectionStart.addEventListener("animationend", event => {
+      if(event.animationName == "down-start")
+        html.sectionStart.style.display = "none";
+        html.body.style.overflow = "visible";
+    });
   }
-});
+  return {
+    init
+  }
+}
 
-sectionStart.addEventListener("animationend", event => {
-  if(event.animationName == "down-start")
-    sectionStart.style.display = "none";
-    document.querySelector("body").style.overflow = "visible";
+window.addEventListener('load', () => {
+  const startPage = StartPage();
+  startPage.init();
 });
